14ce23e1e73b3c07acbb7cf6e7add841981bf137
jefolidev <jefoliveira279@gmail.com>
2025-07-26 13:11:30 -0300

    fix: fixed docker and docker compose files

---
+++ backend/start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Instalando dependências..."
+npm install
+
+echo "Gerando client Prisma..."
+npx prisma generate
+
+echo "Esperando o banco de dados..."
+while ! nc -z pg 5432; do
+  echo "Ainda não conectou no banco... tentando de novo."
+  sleep 1
+done
+
+echo "Banco encontrado! Aplicando migrations..."
+npx prisma migrate deploy
+
+echo "Subindo aplicação..."
+npm start
+echo "Aplicação iniciada com sucesso!"
