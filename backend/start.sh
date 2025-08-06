48a0102171d8d17a1443221152de6b688ba2ce4b
jefolidev <jefoliveira279@gmail.com>
2025-08-06 18:08:34 -0300

    added netcat

--- backend/start.sh
+++ backend/start.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-echo "Instalando dependências..."
-npm install
-
-echo "Gerando client Prisma..."
-npx prisma generate
-
-echo "Esperando o banco de dados..."
+echo "Esperando o banco de dados (Postgres)..."
 while ! nc -z pg 5432; do
   echo "Ainda não conectou no banco... tentando de novo."
   sleep 1
 done
 
-echo "Banco encontrado! Aplicando migrations..."
+echo "Esperando o Redis..."
+while ! nc -z redis 6379; do
+  echo "Ainda não conectou no Redis... tentando de novo."
+  sleep 1
+done
+
+echo "Banco e Redis prontos! Instalando dependências..."
+npm install
+
+echo "Gerando client Prisma..."
+npx prisma generate
+
+echo "Aplicando migrations..."
 npx prisma migrate deploy
 
 echo "Subindo aplicação..."
