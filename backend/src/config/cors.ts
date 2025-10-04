639bfddc699f42cd7ce2d2830da5ece4607082d8
JefersonFranco <157678051+jefolidev@users.noreply.github.com>
2025-10-03 21:00:17 -0300

    update: added new url to cors

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -2,6 +2,7 @@ const allowedOriginsDev = "*"; // libera tudo em dev
 
 const allowedOriginsProd = [
     "http://nwokg48csogs8kk8gckc84ww.31.97.92.35.sslip.io",
+    "http://vg08ss0048wkscsos4gosg08.72.60.241.250.sslip.io",
     "http://fme.bongdigital.com.br",
     "https://fme.bongdigital.com.br",
 ];
