6a4beb3777df3febeb052ed14c0cb372fadc2ac9
JefersonFranco <157678051+jefolidev@users.noreply.github.com>
2025-10-05 16:17:17 -0300

    updated cors

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,7 +1,7 @@
 import cors from 'cors'
 
 export const corsMiddleware = cors({
-  origin: ['http://localhost:5173', 'http://vg08ss0048wkscsos4gosg08.72.60.241.250.sslip.io'],
+  origin: 'http://vg08ss0048wkscsos4gosg08.72.60.241.250.sslip.io',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
   credentials: true,
